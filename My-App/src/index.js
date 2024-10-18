6e7fbc4f0f42a1b00ad59ea26a3b338785c0213a
sung1842 <sungwoo0723@naver.com>
2024-10-18 22:17:29 +0900

    new file:   My-App/src/chapter_08/ConfirmButton.jsx new file:   My-App/src/chapter_09/LandingPage.jsx new file:   My-App/src/chapter_09/Toolbar.jsx modified:   My-App/src/index.js

--- My-App/src/index.js
+++ My-App/src/index.js
@@ -7,12 +7,14 @@ import Clock from './chapter_04/Clock';
 import CommentList from './chapter_05/CommentList';
 import NotificationList from './chapter_06/NotificationList';
 import Accommodate from './chapter_07/Accommodate';
+import ConfirmButton from './chapter_08/ConfirmButton';
+import LandingPage from './chapter_09/LandingPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <Accommodate/>
+    <LandingPage/>
   </React.StrictMode>
 );
 
