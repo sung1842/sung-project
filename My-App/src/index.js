2c4e883ce6c0fb350cd4bac4c4439619fe574243
sung1842 <sungwoo0723@naver.com>
2024-10-28 18:27:28 +0900

    modified:   My-App/package-lock.json modified:   My-App/package.json new file:   My-App/src/chapter_10/AttendanceBook.jsx new file:   My-App/src/chapter_11/SignUp.jsx new file:   My-App/src/chapter_12/Calculator.jsx new file:   My-App/src/chapter_12/TemperatureInput.jsx new file:   My-App/src/chapter_13/Card.jsx new file:   My-App/src/chapter_13/ProfileCard.jsx new file:   My-App/src/chapter_14/DarkOrLight.jsx new file:   My-App/src/chapter_14/MainContent.jsx new file:   My-App/src/chapter_14/ThemeContext.jsx new file:   My-App/src/chapter_15/Blocks.jsx modified:   My-App/src/index.js

--- My-App/src/index.js
+++ My-App/src/index.js
@@ -9,12 +9,18 @@ import NotificationList from './chapter_06/NotificationList';
 import Accommodate from './chapter_07/Accommodate';
 import ConfirmButton from './chapter_08/ConfirmButton';
 import LandingPage from './chapter_09/LandingPage';
+import AttendanceBook from './chapter_10/AttendanceBook';
+import SignUp from './chapter_11/SignUp';
+import Calculator from './chapter_12/Calculator';
+import ProfileCard from './chapter_13/ProfileCard';
+import DarkOrLight from './chapter_14/DarkOrLight';
+import Blocks from './chapter_15/Blocks';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <LandingPage/>
+    <Blocks/>
   </React.StrictMode>
 );
 
